0d31625948c00571d7288f35dd1eaf03f063b8bb
Kulbhushan.Saxena <Kulbhushan.Saxena@thomsonreuters.com>
2020-01-31 21:10:51 +0530

    MVC Test

--- Spring5-Recipe-App/src/test/java/com/recipe/controllers/IndexControllerTest.java
+++ Spring5-Recipe-App/src/test/java/com/recipe/controllers/IndexControllerTest.java
@@ -5,6 +5,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +17,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.recipe.domain.Recipe;
@@ -51,7 +56,7 @@ public class IndexControllerTest {
         when(recipeService.getRecipes()).thenReturn(recipes);
 
         //Use it to capture argument values for further assertions
-        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         //when
         String viewName = controller.indexPage(model);
@@ -63,6 +68,23 @@ public class IndexControllerTest {
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         Set<Recipe> setInController = argumentCaptor.getValue();
         assertEquals(2, setInController.size());
+    }
+    
+    /*
+     * Testing MVCs
+     */
+    @Test
+    public void testMockMVC() throws Exception{
+    	/*
+    	 * 2 variations :
+    	 * webAppContextSetup - used in integration test as it loads the spring context as well
+    	 * standaloneSetup - for unit Test
+    	 */
+    	MockMvc mockMVC = MockMvcBuilders.standaloneSetup(controller).build();
+    	
+    	mockMVC.perform(get("/"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("index"));
     }
 
 }
